f3a39e87edf9f1f7dadabc17eaa3a09cfe6b4811
Yinghui Dong <rakal.dongyinghui@gmail.com>
2018-06-28 17:05:37 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# cs4641project
---
+++ loadMNIST.py
@@ -0,0 +1,46 @@
+import os, struct
+from array import array
+from cvxopt.base import matrix
+import numpy as np
+
+def read(digits, dataset = "training", path = "MNIST_data"):
+    """
+    Python function for importing the MNIST data set.
+    """
+
+    if dataset is "training":
+        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+    elif dataset is "testing":
+        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+    else:
+        raise ValueError, "dataset must be 'testing' or 'training'"
+
+    flbl = open(fname_lbl, 'rb')
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = array("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = array("B", fimg.read())
+    fimg.close()
+
+    ind = [ k for k in xrange(size) if lbl[k] in digits ]
+    images =  matrix(0, (len(ind), rows*cols))
+    labels = matrix(0, (len(ind), 1))
+    for i in xrange(len(ind)):
+        images[i, :] = img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]
+        labels[i] = lbl[ind[i]]
+
+    return images, labels
+
+[images,label]=read([1])
+images1=images[0,0:784]
+label1=label[1:2]
+print(images1)
+print(label1)
+data=np.array(images1)
+data.reshape((28,28))
+print data()
