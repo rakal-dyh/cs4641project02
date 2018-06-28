c367bcd1fbe8426f50df0da0ee511e6193bb3049
Yinghui Dong <rakal.dongyinghui@gmail.com>
2018-06-28 18:08:14 -0400

    frame

---
+++ NNModelTrain.py
@@ -0,0 +1,24 @@
+from loadMNIST import read
+
+class NNModel():
+    data=[]
+    label=[]
+
+    testdata=[]
+    testlabel=[]
+
+    def readData():
+        pass
+
+    def trainModel():
+        pass
+
+    def predict():
+        pass
+
+
+
+if __name__=="__main__":
+    NNmodel1=NNModel()
+
+    pass
