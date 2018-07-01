56143a802c7d6e55db2df5374d71d12a8c3cbae5
Yinghui Dong <rakal.dongyinghui@gmail.com>
2018-07-01 17:08:32 -0400

    frame

--- NNModelTrain.py
+++ NNModelTrain.py
@@ -1,23 +1,70 @@
 from loadMNIST import read
+import numpy as np
 
 class NNModel():
     data=[]
     label=[]
+    labelFormatted[]
 
     testdata=[]
     testlabel=[]
+    testlabelFormatted[]
 
-    #
+    #this method read the images and labels
     def readData(self,numberOfRows):
         self.data=read(3)
         print data
         pass
 
-    def trainModel():
+    #use the data to train the model
+    def trainModel(self):
         pass
 
-    def predict():
+    #predict new data points with model
+    #retrun a vector with 10 columns
+    '''
+    ex: (new format)
+        1,0,0,0,0,0,0,0,0,0
+        0,0,0,0,1,0,0,0,0,0
+        ...
+    it is (old format)
+        0
+        4
+    '''
+    def predict(self):
         pass
+
+    #calculate_loss
+    def calculate_loss(self):
+        pass
+
+    #using test data to check the accuracy
+    def validation(self):
+        pass
+
+    #change from origin label to new formatted label
+    '''
+        origin label is a vector like:
+        [0,5,2,4,...]
+        new formatted label is:
+        1,0,0,0,0,0,0,0,0,0
+        0,0,0,0,0,1,0,0,0,0
+        0,0,1,0,0,0,0,0,0,0
+        0,0,0,0,1,0,0,0,0,0
+    '''
+    def changeLabelFormatToNew(self):
+        pass
+
+#Util:
+
+    @staticmethod
+    def sigmoid(a):
+        return 1/(1+np.exp(-a))
+
+    @staticmethod
+    def sigmoid_derivative(a):
+        out=np.multiply(sigmoid(a),(1-sigmoid(a)))
+    return out
 
 
 
